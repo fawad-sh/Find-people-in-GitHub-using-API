9058fcaa8ad2d2265ad4bc5276b0abaa3fb74d75
fawad sheikh <fawad313@yahoo.com>
2021-07-09 10:49:24 +0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import { Alert } from './components/layout/Alert';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    users: [],
+    loading: false,
+    alert: null,
+  }
+
+  // async componentDidMount() {
+  //   this.state.loading = true;
+  //   this.setState({ loading: true });
+    
+  //   const res = await fetch(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  //   const data = await res.json();
+
+  //   this.setState({ users: data, loading: false });
+  // }
+
+  // Search users in githib repository
+  searchUsers = async (text) => {
+    this.state.loading = true;
+
+    const res = await fetch(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    const data = await res.json();
+    console.log(data);
+    this.setState({ users: data.items, loading: false });
+  }
+
+  // clear users list from the page(UI)
+  clearUsers = () => this.setState({ users: [], loading: false,})
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: {msg: msg, type: type}});
+
+    setTimeout(() => this.setState({alert: null}), 5000);
+  }
+  
+  render() {
+    return (
+      <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Search 
+            searchUsers={this.searchUsers} 
+            clearUsers={this.clearUsers} 
+            showClear={this.state.users.length > 0 ? true: false}
+            setAlert={this.setAlert}
+            />
+            <Users users={this.state.users} loading={this.state.loading}/>
+          </div>
+      </div>
+    );
+    }
 }
 
 export default App;
---
+++ src/components/users/Users.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import UserItem from './UserItem';
+import PropTypes from 'prop-types'
+
+ const Users = ({ users, loading}) => {
+    if (loading) {
+        return (<h2 className='text-center'>Loading</h2>)
+    } else {
+        return (
+            <div style={userStyle}>
+                {users.map( user => (
+                    <UserItem key={user.id} user={user} />  
+                ))}
+            </div>
+        )
+    }
+}
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
+}
+const userStyle = {
+    display : 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem',
+}
+export default Users
